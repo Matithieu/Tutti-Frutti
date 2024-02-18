3de6dcd57fa8004f1231c2bbe82baae97e7bc174
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-02-18 17:23:35 +0100

    feat: release

--- src/Service/DiscogsService.php
+++ src/Service/DiscogsService.php
@@ -37,6 +37,13 @@ class DiscogsService
         return $response->toArray();
     }
 
+    public function getRelease($id)
+    {
+        $url = 'releases/' . $id . '?token=' . $this->token;
+        $response = $this->client->request('GET', $url);
+        return $response->toArray();
+    }
+
     public function search(string $query, int $page = 1, int $limit = 10): array
     {
         $url = 'database/search?q=' . $query . '&type=all' . '&token=' . $this->token . '&per_page=' . $limit . '&page=' . $page;
