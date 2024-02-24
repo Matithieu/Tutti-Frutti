48d5a29206517bb6ab13fb856219a6cd3f734ffe
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-02-24 23:22:20 +0100

    feat: add enums

--- src/Entity/Enum/GenreEnum.php
+++ src/Entity/Enum/GenreEnum.php
@@ -5,8 +5,40 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Embeddable]
 class GenreEnum
 {
-    public const ROCK = 'Rock';
-    public const POP = 'Pop';
+    // https://support.discogs.com/hc/fr/articles/360005055213-Guide-d-Utilisation-de-la-Base-de-Donn%C3%A9es-9-Genres-Styles
+    /**
+     * Blues
+Brass & Military(Fanfares & Militaires)
+Pour Enfant
+Classique
+Électronique
+Folk, World, & Country(Folk, Musique du Monde, & Country)
+Funk / Soul
+Funk / Soul
+Hip-Hop
+Jazz
+Latin
+Pas de music
+Pop
+Reggae
+Rock 
+Stage & Screen 
+     */
+    public const BLUES = 'Blues';
+    public const BRASS_AND_MILITARY = 'Brass_and_military';
+    public const CHILDREN = 'Children';
+    public const CLASSICAL = 'Classical';
+    public const ELECTRONIC = 'Electronic';
+    public const FOLK_WORLD_COUNTRY = 'folk, world, & country';
+    public const FUNK_SOUL = 'funk_soul';
+    public const HIP_HOP = 'hip hop';
+    public const JAZZ = 'jazz';
+    public const LATIN = 'latin';
+    public const NO_MUSIC = 'no_music';
+    public const POP = 'pop';
+    public const REGGAE = 'reggae';
+    public const ROCK = 'rock';
+    public const STAGE_AND_SCREEN = 'stage_and_screen';
     // Add more genres as needed
 
     #[ORM\Column(type: 'string', length: 255)]
