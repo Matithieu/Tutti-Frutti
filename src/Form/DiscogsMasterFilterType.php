ae5fdafaf1e9f5a745132f99eca5b05c0da784f0
Mathieu <mathieu.yahia-amar@etu.univ-amu.fr>
2024-02-25 12:12:45 +0100

    fix: remove not working filters

--- src/Form/DiscogsMasterFilterType.php
+++ src/Form/DiscogsMasterFilterType.php
@@ -6,6 +6,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DiscogsMasterFilterType extends AbstractType
@@ -17,18 +19,40 @@ class DiscogsMasterFilterType extends AbstractType
                 'choices' => [
                     'Fruit' => 'fruit',
                     'Année' => 'year',
-                    'Nom du groupe' => 'groupName',
-                    'Label' => 'label',
-                    'Genre' => 'genre',
-                    'Format' => 'format',
+                    //'Nom du groupe' => 'groupName',
+                    //'Label' => 'label',
+                    //'Genre' => 'genre',
+                    //'Format' => 'format',
                 ],
                 'label' => 'Choisir le type de filtre',
                 'placeholder' => 'Sélectionnez un filtre',
             ])
+
             ->add('filterValue', TextType::class, [
-                'label' => 'Valeur de filtre',
+                'label' => 'Valeur du filtre',
                 'required' => false,
             ]);
+
+        // Debug: Data is null idk why. function not working. But the idea is here.
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            $data = $event->getData();
+
+            if ($data != null && $data['filterType'] === 'fruit') {
+                $form->add('filterValue', ChoiceType::class, [
+                    'choices' => [
+                        'Banane' => 'banane',
+                        'Pomme' => 'pomme',
+                        'Poire' => 'poire',
+                        'Fraise' => 'fraise',
+                        'Orange' => 'orange',
+                    ],
+                    'label' => 'Valeur du filtre',
+                    'placeholder' => 'Sélectionnez une valeur',
+                    'required' => false,
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
